cc568fc81d0d20a763c2b3e1983ea366adacb254
lan.ho <lan.ho@asnet.com.vn>
2025-11-18 15:33:50 +0700

    feat(activity): implement activity from react19.2

---
+++ my-app/app/activity/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Activity, useState } from "react";
+import Sidebar from "../components/sidebar";
+
+export default function Home() {
+  const [isShowingSidebar, setIsShowingSidebar] = useState(true);
+
+  return (
+    <div className="flex flex-row">
+      <div className="flex flex-row">
+        <p>Old sidebar</p>
+        {isShowingSidebar && <Sidebar />}
+      </div>
+      <div className="flex flex-row">
+        <p>Sidebar with Activity</p>
+        <Activity mode={isShowingSidebar ? "visible" : "hidden"}>
+          <Sidebar />
+        </Activity>
+      </div>
+
+      <main>
+        <button onClick={() => setIsShowingSidebar(!isShowingSidebar)}>
+          Toggle sidebar
+        </button>
+        <h1>Main content</h1>
+      </main>
+    </div>
+  );
+}
